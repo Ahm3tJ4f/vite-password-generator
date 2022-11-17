1842df2b85ce5e0c365bd0364a2012be96a3c1d2
Ahmed Caferov <ahmetleo56@gmail.com>
2022-11-17 19:57:46 +0400

    initialize tailwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 const App = () => {
-  return <div></div>;
+  return (
+    <div className="bg-rose-400">
+      <div className="w-10 h-10 bg-red ">hi</div>
+    </div>
+  );
 };
 
 export default App;
