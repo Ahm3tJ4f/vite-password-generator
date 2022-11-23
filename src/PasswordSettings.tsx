4ad9af0d00eeac55f113da36ec1ea71b9c7aff21
Ahmed Caferov <ahmetleo56@gmail.com>
2022-11-23 23:50:59 +0400

    style range slider with styled-components

--- src/PasswordSettings.tsx
+++ src/PasswordSettings.tsx
@@ -1,4 +1,22 @@
 import { useState } from "react";
+import styled from "styled-components";
+
+const StyledRange = styled.input.attrs({ type: "range" })`
+  &::-webkit-slider-runnable-track {
+    width: 100%;
+    height: 10px;
+    cursor: pointer;
+    border-radius: 5px;
+
+    background: linear-gradient(
+      to right,
+      #a4ffaf 0%,
+      #a4ffaf ${(length / 20) * 100 + "%"},
+      white ${(length / 20) * 100 + "%"},
+      white 100%
+    );
+  }
+`;
 
 const PasswordSettings = () => {
   const [length, setLength] = useState(10);
@@ -11,13 +29,13 @@ const PasswordSettings = () => {
     <form className="contentBox flex flex-col space-y-8">
       <label
         className="flex justify-between items-center"
-        htmlFor="password-length "
+        htmlFor="password-length"
       >
         <span className=" ">Character Length</span>
         <span className="text-primaryGreen text-3xl">{length}</span>
       </label>
 
-      <input
+      <StyledRange
         onChange={handleRange}
         type="range"
         name="password-length"
--- src/PasswordTable.tsx
+++ src/PasswordTable.tsx
@@ -1,24 +1,39 @@
+import styled from "styled-components";
 interface Props {
   generatedPassword: string;
 }
+
+const ContentBox = styled.div`
+  display: flex;
+  border-radius: 1rem;
+  justify-content: space-between;
+  width: 35rem;
+  background-color: #2b2a33;
+  padding: 1.75rem 2rem;
+`;
+
+const Password = styled.span`
+  font-size: 1.875rem /* 30px */;
+  line-height: 2.25rem /* 36px */;
+`;
+
+const CopyButton = styled.img`
+  height: 1.75rem;
+  filter: invert(89%) sepia(12%) saturate(1160%) hue-rotate(66deg)
+    brightness(102%) contrast(102%);
+`;
 // eslint-disable-next-line react/prop-types
 const PasswordTable: React.FC<Props> = ({ generatedPassword }) => {
   const handleClick = () => {
     navigator.clipboard.writeText(generatedPassword);
   };
   return (
-    <div className="contentBox justify-between">
-      <span className="text-3xl">
-        {generatedPassword ? generatedPassword : "P4S5w0Rd"}
-      </span>
+    <ContentBox>
+      <Password>{generatedPassword ? generatedPassword : "P4S5w0Rd"}</Password>
       <button onClick={handleClick}>
-        <img
-          className="h-7 filler-green"
-          src="copy-regular.svg"
-          alt="copy icon"
-        />
+        <CopyButton src="copy-regular.svg" alt="copy icon" />
       </button>
-    </div>
+    </ContentBox>
   );
 };
 export default PasswordTable;
